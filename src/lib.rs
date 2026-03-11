6475fca84193f9bcb6aa24378a3ad6726a71ac49
version513 <seuair@gmail.com>
2026-03-11 17:56:33 +0800

    refactor: move points module into separate file

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,7 @@
 pub mod backends;
 pub mod curves;
+pub mod points;
 pub mod drand;
 pub mod examples;
 pub mod kyber;
 pub mod traits;
-
-pub mod points {
-    use crate::traits::Group;
-    use crate::traits::Scheme;
-
-    /// Keygroup point in affine representation
-    pub type KeyPoint<S> = <<S as Scheme>::Key as Group>::Affine;
-
-    /// Siggroup point in affine representation
-    pub type SigPoint<S> = <<S as Scheme>::Sig as Group>::Affine;
-
-    /// Keygroup point in projective representation
-    pub type KeyPointProjective<S> = <<S as Scheme>::Key as Group>::Projective;
-
-    /// Siggroup point in projective representation
-    pub type SigPointProjective<S> = <<S as Scheme>::Sig as Group>::Projective;
-}
---
+++ src/points.rs
@@ -0,0 +1,14 @@
+    use crate::traits::Group;
+    use crate::traits::Scheme;
+
+    /// Keygroup point in affine representation
+    pub type KeyPoint<S> = <<S as Scheme>::Key as Group>::Affine;
+
+    /// Siggroup point in affine representation
+    pub type SigPoint<S> = <<S as Scheme>::Sig as Group>::Affine;
+
+    /// Keygroup point in projective representation
+    pub type KeyPointProjective<S> = <<S as Scheme>::Key as Group>::Projective;
+
+    /// Siggroup point in projective representation
+    pub type SigPointProjective<S> = <<S as Scheme>::Sig as Group>::Projective;
